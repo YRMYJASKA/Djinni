1f7c29b9f95a6e4781ee9cf9c029aa5533063260
Jyry Hjelt <hjelt.jyry@gmail.com>
2018-03-01 17:47:07 +0100

    0.2.1: Fixed #1 and added comments to screen.cpp
    Changelog:
    - Fixed bug #1 which was affecting the resizing of the window
    - Added some comments to explain the file src/screen.cpp

--- include/version.h
+++ include/version.h
@@ -1,5 +1,5 @@
 #pragma once
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 2
-#define VERSION_PATCH 0
-static const char* VERSION_STRING = "0.2.0";
+#define VERSION_PATCH 1
+static const char* VERSION_STRING = "0.2.1";
